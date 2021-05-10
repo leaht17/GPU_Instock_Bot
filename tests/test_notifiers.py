df7ce0d1e1bedacc7279f79ebb74add270c5d628
Leah <41168640+leaht17@users.noreply.github.com>
2021-05-10 12:57:13 -0700

    Modified test to use testing credentials when possible.

--- tests/test_notifiers.py
+++ tests/test_notifiers.py
@@ -17,29 +17,35 @@ class TestScraper(unittest.TestCase):
 
         # Twilio client set up using live credentials (since testing functionality is limited)
         self.client = Client('AC9d5de46a73c27da46f9c0de98f668e20', '0000ab4bffd746f96c75e19fe9a52079')
+        self.test_client = Client('AC56538a2f99e2863f36cdea0143417e2c', '2ffe0243e02fbaf672773e32dd260ccc')
 
     # TODO: add tests for email notifications after implementation is done
-    """ Tests if sends correct email message. """
-    def test_email_send_message(self):
-        print("test_email_send_message not yet implemented")
+    # """ Tests if sends correct email message. """
+    # def test_email_send_message(self):
+    #     print("test_email_send_message not yet implemented")
 
-    # note that this test actually sends a text, using our live credentials
-    """ Tests if sends correct text message. """
+    """ Tests if correctly sends text message. """
     def test_send_text(self):
+        # note that this test actually sends a text, using our live credentials
+        
         trial_message = "Sent from your Twilio trial account - "
         url = "Test_GPU_Instock_Bot_URL"
         stock_message = "Message successfully sent for test_notifiers.test_send_text."
-        phone_number = '2068593002' # current set to Leah's number for testing
-        # phone_number = '5005550006' # valid Twilio magic number (doesn't work with live credentials)
-
+        valid_unavailable_phone_number = '5005550006' # valid, unavailable magic number
         expected = trial_message + stock_message + "\n\n" + url
+        
+        # expects no error in sending message
+        send_text(url, stock_message, self.test_client, valid_unavailable_phone_number)
 
-    #     send_text(url, stock_message, self.client, phone_number, sender_phone_num='+16782632233')
-    #     messages = self.client.messages.list()
-    #     print("body: " + messages[0].body)
-    #     result = messages[0].body
+        # commented out for now because it actually sends text
+        # valid_phone_number = '2068593002' # currently set to Leah's number for testing
+        # send_text(url, stock_message, self.client, valid_phone_number, sender_phone_num='+16782632233')
+        # messages = self.client.messages.list()
+        # print("body: " + messages[0].body)
+        # result = messages[0].body
 
-    #     self.assertEqual(result, expected)
+        # compares message sent with expected message
+        # self.assertEqual(result, expected)
 
     """ Tests if exception raised when given invalid number. """
     def test_send_text_invalid_number(self):
@@ -50,12 +56,13 @@ class TestScraper(unittest.TestCase):
 
         # uses testing credentials
         self.assertRaises(twilio.base.exceptions.TwilioRestException, \
-            send_text, url, stock_message, self.client, invalid_phone_number)
+            send_text, url, stock_message, self.test_client, invalid_phone_number)
         
     # note: query_module() error - could not connect to server
-    def test_query_module(self):
-        """ Tests if retrieves correct emails/phone numbers from database. """
-        # list_of_emails, list_of_phone_numbers = query_module("fake_url")
+    # def test_query_module(self):
+    #     """ Tests if retrieves correct emails/phone numbers from database. """
+    #     test_query_module not yet implemented
+    #     list_of_emails, list_of_phone_numbers = query_module("fake_url")
     #     self.assertEqual(list_of_emails, [])
     #     self.assertEqual(list_of_phone_numbers, [])
 
