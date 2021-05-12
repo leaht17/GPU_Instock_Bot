517f9738bd9056412e76429be202f51e75f96688
argonaeut <derekjustinc@yahoo.com>
2021-05-11 17:54:02 -0700

    updated the .env cmd

--- setupWindows.sh
+++ setupWindows.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-py -m venv .env && source .\.env\Scripts\activate && pip install -r requirements.txt && cd GPU_Instock_Bot_V2_src && chmod u+x setupdb.sh && ./setupdb.sh && cd ..
+py -m venv .env && source .env/Scripts/activate && pip install -r requirements.txt && cd GPU_Instock_Bot_V2_src && chmod u+x setupdb.sh && ./setupdb.sh && cd ..
